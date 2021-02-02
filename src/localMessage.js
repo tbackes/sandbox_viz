16b37b9a4a79c143bfb3aef0a5c272e595904f0d
Tracy Backes <tbackes@twitter.com>
2021-02-02 15:28:42 -0700

    Initialize sandbox

---
+++ src/localMessage.js
@@ -0,0 +1,258 @@
+/**
+ * This file provides the mock "data" received
+ * by your visualization code when you develop
+ * locally.
+ *
+ */
+export const message = {
+  "tables": {
+    "DEFAULT": [
+      {
+        "dimension": [
+          "0-14"
+        ],
+        "metric": [
+          "1"
+        ]
+      },
+      {
+        "dimension": [
+          "15-24"
+        ],
+        "metric": [
+          "1"
+        ]
+      },
+      {
+        "dimension": [
+          "25-54"
+        ],
+        "metric": [
+          "1"
+        ]
+      },
+      {
+        "dimension": [
+          "55-64"
+        ],
+        "metric": [
+          "1"
+        ]
+      },
+      {
+        "dimension": [
+          "65+"
+        ],
+        "metric": [
+          "1"
+        ]
+      }
+    ]
+  },
+  "fields": {
+    "dimension": [
+      {
+        "id": "qt_yvecch1jgc",
+        "name": "Age Group",
+        "type": "TEXT",
+        "concept": "DIMENSION"
+      }
+    ],
+    "metric": [
+      {
+        "id": "qt_zvecch1jgc",
+        "name": "Record Count",
+        "type": "NUMBER",
+        "concept": "METRIC"
+      }
+    ]
+  },
+  "style": {
+    "barColor": {
+      "value": {
+        "color": "#222222"
+      },
+      "defaultValue": {
+        "color": "#222222"
+      }
+    }
+  },
+  "theme": {
+    "themeFillColor": {
+      "color": "#ffffff",
+      "themeRef": {
+        "index": 0
+      }
+    },
+    "themeFontColor": {
+      "color": "#000000",
+      "themeRef": {
+        "index": 1
+      }
+    },
+    "themeFontFamily": "Roboto",
+    "themeAccentFillColor": {
+      "color": "#e0e0e0",
+      "themeRef": {
+        "index": 2
+      }
+    },
+    "themeAccentFontColor": {
+      "color": "#000000",
+      "themeRef": {
+        "index": 3
+      }
+    },
+    "themeAccentFontFamily": "Roboto",
+    "themeSeriesColor": [
+      {
+        "color": "#0072f0",
+        "seriesRef": {
+          "index": 0
+        },
+        "themeRef": {
+          "index": 4
+        }
+      },
+      {
+        "color": "#00b6cb",
+        "seriesRef": {
+          "index": 1
+        },
+        "themeRef": {
+          "index": 5
+        }
+      },
+      {
+        "color": "#f10096",
+        "seriesRef": {
+          "index": 2
+        },
+        "themeRef": {
+          "index": 6
+        }
+      },
+      {
+        "color": "#f66d00",
+        "seriesRef": {
+          "index": 3
+        },
+        "themeRef": {
+          "index": 7
+        }
+      },
+      {
+        "color": "#ffa800",
+        "seriesRef": {
+          "index": 4
+        },
+        "themeRef": {
+          "index": 8
+        }
+      },
+      {
+        "color": "#7cb342",
+        "seriesRef": {
+          "index": 5
+        },
+        "themeRef": {
+          "index": 9
+        }
+      },
+      {
+        "color": "#5e35b1",
+        "seriesRef": {
+          "index": 6
+        }
+      },
+      {
+        "color": "#03a9f4",
+        "seriesRef": {
+          "index": 7
+        }
+      },
+      {
+        "color": "#ec407a",
+        "seriesRef": {
+          "index": 8
+        }
+      },
+      {
+        "color": "#ff7043",
+        "seriesRef": {
+          "index": 9
+        }
+      },
+      {
+        "color": "#737373",
+        "seriesRef": {
+          "index": 10
+        }
+      },
+      {
+        "color": "#f15a60",
+        "seriesRef": {
+          "index": 11
+        }
+      },
+      {
+        "color": "#7ac36a",
+        "seriesRef": {
+          "index": 12
+        }
+      },
+      {
+        "color": "#5a9bd4",
+        "seriesRef": {
+          "index": 13
+        }
+      },
+      {
+        "color": "#faa75a",
+        "seriesRef": {
+          "index": 14
+        }
+      },
+      {
+        "color": "#9e67ab",
+        "seriesRef": {
+          "index": 15
+        }
+      },
+      {
+        "color": "#ce7058",
+        "seriesRef": {
+          "index": 16
+        }
+      },
+      {
+        "color": "#d77fb3",
+        "seriesRef": {
+          "index": 17
+        }
+      },
+      {
+        "color": "#81d4fa",
+        "seriesRef": {
+          "index": 18
+        }
+      },
+      {
+        "color": "#f48fb1",
+        "seriesRef": {
+          "index": 19
+        }
+      }
+    ],
+    "themeIncreaseColor": {
+      "color": "#388e3c"
+    },
+    "themeDecreaseColor": {
+      "color": "#f44336"
+    },
+    "themeGridColor": {
+      "color": "#d1d1d1"
+    }
+  },
+  "interactions": {}
+};
